ac8619ee76981d64dd14eb1c0459c7a1542d9b3a
vgoel <59496579+goelvaibhav105@users.noreply.github.com>
2022-10-03 11:19:57 +0530

    some responsive and random banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -21,7 +21,16 @@ export default function Banner() {
       // eslint-disable-next-line
   }, [])
   if(loading){
-    return null
+     return (
+      <div>
+          <img src={'https://images.unsplash.com/photo-1651604454911-fdfb0edde727?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjgzMjR8MHwxfGFsbHwyMDk4Mnx8fHx8fDJ8fDE2NjQ3NzU4NTk&ixlib=rb-1.2.1&q=80&w=1080'} className="w-[100%] h-[500px] mt-[-20px] object-cover"  alt="" />
+          <div className='absolute top-[30%] left-[15%] backdrop-blur-2xl bg-white/1  h-[250px] w-[70%] p-[3%]'>
+              <h1 className='font-[700] text-[55px] text-white'>Unsplash</h1>
+              <p className=' text-[20px] text-white'>The internet’s source for visuals.</p>
+              <p className=' text-[20px] text-white'>Powered by creators everywhere</p>
+          </div>
+      </div>
+    )
 }
 
 const coverImage = results.urls.regular
@@ -29,7 +38,7 @@ const coverImage = results.urls.regular
   return (
     <div>
         <img src={coverImage} className="w-[100%] h-[500px] mt-[-20px] object-cover"  alt="" />
-        <div className='absolute top-[50%] left-[15%] backdrop-blur-2xl bg-white/1  h-[250px] w-[70%] p-[3%]'>
+        <div className='absolute top-[30%] left-[15%] backdrop-blur-2xl bg-white/1  h-[250px] w-[70%] p-[3%]'>
             <h1 className='font-[700] text-[55px] text-white'>Unsplash</h1>
             <p className=' text-[20px] text-white'>The internet’s source for visuals.</p>
             <p className=' text-[20px] text-white'>Powered by creators everywhere</p>
